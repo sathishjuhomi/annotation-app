d5e3953f09ebe618e7aa741327e2885505fcad28
sathishjuhomi <sathishk@juhomi.com>
2023-09-11 16:25:13 +0530

    variable names changed in the files

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from backend.routers.user_login import auth_router
+from backend.routers.user import auth_router
 
 app = FastAPI()
 
