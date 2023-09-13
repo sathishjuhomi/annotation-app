55cc197bdeb1a014a4e490a007924438f53a9a7a
ukmurali123 <murali@juhomi.com>
2023-09-13 13:09:41 +0530

    Code refactor done

--- maverick-frontend/src/app/forgot-password/api/route.ts
+++ maverick-frontend/src/app/forgot-password/api/route.ts
@@ -1,4 +1,4 @@
-import { FormData } from "../interfaces";
+import { FormData } from "../../component/interfaces";
 
 export async function forgotPassword(formData: FormData) {
   const email = formData.email;
