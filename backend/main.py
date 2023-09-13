43a2a9969d739427fef7b1c023fc687ef07a9597
sathishjuhomi <sathishk@juhomi.com>
2023-09-13 17:32:38 +0530

    added logger to main.py

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
+import logging
 
 from backend.routers.user import auth_router
 from backend.routers.oauth import oauth_router
+
+logger = logging.getLogger(__name__) 
+fileHandler = logging.FileHandler("logs.txt")
+
+stdoutFmt = logging.Formatter(
+    "%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s"
+)
+
+logging.basicConfig(
+    level = logging.INFO,  # Set the logging level to INFO, you can change it as needed.
+    format = "%(name)s: %(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(process)d >>> %(message)s"
+)
+fileHandler.setFormatter(stdoutFmt)
+logger.addHandler(fileHandler)
+
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="!secret")
 app.add_middleware(
@@ -16,3 +32,5 @@ app.add_middleware(
 
 app.include_router(auth_router)
 app.include_router(oauth_router)
+
+logger.info("Executed main.py")
