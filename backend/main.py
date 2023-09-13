7532d9e92a4668df7541c468a9ceb1b529f69af0
sathishjuhomi <sathishk@juhomi.com>
2023-09-13 13:53:39 +0530

    merged feature/authentication-backend with v2/check_oauth_error

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
-from backend.routers.user_login import auth_router
-
+from backend.routers.user import auth_router
+from backend.routers.oauth import oauth_router
 app = FastAPI()
 app.add_middleware(SessionMiddleware, secret_key="!secret")
 app.add_middleware(
