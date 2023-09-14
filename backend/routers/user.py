440e117371cd093d8422f4d6c3802e25e2b061c9
sathishjuhomi <sathishk@juhomi.com>
2023-09-14 15:35:32 +0530

    added logs to user.py

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -55,11 +55,15 @@ def check_existing_user(db, column_name, value):
 def sign_up(request_payload: UserSchema, db: Session = Depends(get_db)) -> Any:
     existing_user = check_existing_user(db=db, column_name='email', value=request_payload.email)
     if existing_user:
+        logger.warning("User sign-up failed: User with this email already exists")
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail="User with this email already exists",
         )
-    return user_service.create_user(request_payload=request_payload, db=db)
+    
+    user = user_service.create_user(request_payload=request_payload, db=db)
+    logger.info("User sign-up successful for user: %s", user.email)
+    return user
 
 
 @auth_router.post(
@@ -80,10 +84,12 @@ def sign_up(request_payload: UserSchema, db: Session = Depends(get_db)) -> Any:
 def sign_in(request_payload: UserSchema, db: Session = Depends(get_db)) -> Any:
     existing_user = check_existing_user(db=db, column_name='email', value=request_payload.email)
     if not existing_user:
+        logger.warning("User sign-in failed: email id doesn't exists")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email"
         )
     if existing_user.t_delete:
+        logger.warning("Inactive user")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
         )
@@ -91,6 +97,7 @@ def sign_in(request_payload: UserSchema, db: Session = Depends(get_db)) -> Any:
         request_payload, existing_user
     )
     if not valid_password:
+        logger.warning("Incorrect Password")
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect password"
         )
@@ -119,6 +126,7 @@ def sign_in(request_payload: UserSchema, db: Session = Depends(get_db)) -> Any:
 async def recover_password(email: str, db: Session = Depends(get_db)) -> Any:
     user = user_db_handler.load_by_column(db=db, column_name="email", value=email)
     if not user:
+        logger.warning("The user with this email does not exist in the system.")
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="The user with this email does not exist in the system.",
@@ -159,6 +167,7 @@ def reset_password(
 ) -> Any:
     email = verify_password_reset_token(token)
     if not email:
+        logger.warning('Invalid token')
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid token"
         )
