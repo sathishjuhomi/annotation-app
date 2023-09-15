a0c0eb20dec1f5970c445410822e1c8006fcc26a
ukmurali123 <murali@juhomi.com>
2023-09-15 15:39:48 +0530

    Added design for onboarding page

--- maverick-frontend/src/app/onboarding/index.tsx
+++ maverick-frontend/src/app/onboarding/index.tsx
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { useRouter } from "next/navigation";
 
+
 export default function Onboarding() {
   const router = useRouter();
   const navigateToSignIn = () => {
@@ -19,7 +20,12 @@ export default function Onboarding() {
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        display: "flex",
+        backgroundColor: "#303030",
+      }}
+    >
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
@@ -35,17 +41,40 @@ export default function Onboarding() {
               Login
             </Button>
             <Button sx={{ color: "#fff" }} onClick={navigateToSignUp}>
-                Sign Up
+              Sign Up
             </Button>
           </Box>
         </Toolbar>
       </AppBar>
       <Box component="main" sx={{ p: 3 }}>
         <Toolbar />
-        <Typography>
-          Build products faster in Maverick is the best Next.js and Python SaaS
-          template out there. Focus on your business, not on the boilerplate.
+        <Typography
+          sx={{ color: "#fff", marginTop: "30px" }}
+          variant="h2"
+          gutterBottom
+        >
+          Build products faster
         </Typography>
+        <Typography
+          sx={{ color: "#fff", marginTop: "-30px" }}
+          variant="h2"
+          gutterBottom
+        >
+          in Next.js and Python
+        </Typography>
+        <Typography
+          variant="h5"
+          gutterBottom
+          sx={{ color: "#fff", marginTop: "20px" }}
+        >
+          Maverick is the best Next.js and Python SaaS template out there. Focus
+          on your business, not on the boilerplate.
+        </Typography>
+        <br />
+        <br />
+        <Button variant="contained">Maverick your next app</Button>
+        <br />
+        <br />
       </Box>
     </Box>
   );
