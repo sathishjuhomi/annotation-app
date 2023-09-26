56dd3b44663fd3c5bec55e2eb0aa20c5cfae3236
sathishjuhomi <sathishk@juhomi.com>
2023-09-26 19:43:37 +0530

    corrected unittest case for team

--- backend/tests/test_service/test_team.py
+++ backend/tests/test_service/test_team.py
@@ -1,6 +1,5 @@
 import unittest
-from unittest.mock import MagicMock
-from unittest.mock import Mock, patch
+from unittest.mock import MagicMock, patch
 from backend.service.team import team_service
 from backend.schemas.request.team import TeamSchema
 
@@ -15,30 +14,35 @@ class TestTeamService(unittest.TestCase):
 
     @patch("backend.service.user.create_access_token", return_value="mock_token")
     @patch('backend.service.team.get_user_id')
-    @patch('uuid.uuid4')
+    @patch('uuid.uuid4', return_value=TEAM_ID)  # Fix uuid.uuid4 to return a constant value
     @patch('backend.db_handler.team_handler.team_db_handler.create')
     def test_create_team(self, mock_create, mock_uuid4, mock_get_user_id, mock_access_token):
         mock_db = MagicMock()
         mock_request_payload = TeamSchema(team_name="Test Team", token=TOKEN)
-        mock_uuid4.return_value = TEAM_ID
         mock_get_user_id.return_value = USER_ID
+        # Configure the MagicMock to return a dictionary when called
+        mock_create.return_value = {'id': TEAM_ID, 'team_name': 'Test Team', 'created_by': USER_ID}
 
         result = self.team_service.create_team(mock_request_payload, mock_db)
 
-        self.assertEqual(result['id'], TEAM_ID)
-        self.assertEqual(result['team_name'], "Test Team")
-        self.assertEqual(result['created_by'], USER_ID)
+        expected_result = {'id': TEAM_ID, 'team_name': 'Test Team', 'created_by': USER_ID}
+        self.assertEqual(result, expected_result)
 
+    @patch("backend.routers.team.get_team_or_raise_404", return_value="mock_team")
     @patch("backend.service.user.create_access_token", return_value="mock_token")
     @patch('backend.service.team.get_user_id')
     @patch('backend.db_handler.team_handler.team_db_handler.update')
-    def test_update_team(self, mock_update, mock_get_user_id, mock_create_access_token):
+    def test_update_team(self, mock_update, mock_get_user_id, mock_get_team_or_raise_404, mock_create_access_token):
         mock_db = MagicMock()
         mock_request_payload = TeamSchema(team_name="Test Team", token=TOKEN)
         mock_get_user_id.return_value = USER_ID
+        # Configure the MagicMock to return a dictionary when called
+        mock_update.return_value = {'id': TEAM_ID, 'team_name': 'Test Team', 'created_by': USER_ID}
 
-        result = self.team_service.update_team(mock_request_payload, mock_db)
+        result = self.team_service.update_team(mock_request_payload, mock_get_team_or_raise_404, mock_db)
 
-        self.assertEqual(result['id'], TEAM_ID)
-        self.assertEqual(result['team_name'], "Test Team")
-        self.assertEqual(result['created_by'], USER_ID)
+        expected_result = {'id': TEAM_ID, 'team_name': 'Test Team', 'created_by': USER_ID}
+        self.assertEqual(result, expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
