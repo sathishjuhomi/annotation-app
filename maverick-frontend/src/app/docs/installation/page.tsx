e23d5d664ceb73239b0ec69809f094c3e46e4754
unknown <jayabharathi@juhomi.com>
2023-09-27 08:24:39 +0530

    updated the page layout and navbar

--- maverick-frontend/src/app/docs/installation/page.tsx
+++ maverick-frontend/src/app/docs/installation/page.tsx
@@ -1,147 +1,20 @@
 "use client";
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import Drawer from "@mui/material/Drawer";
-import IconButton from "@mui/material/IconButton";
-import List from "@mui/material/List";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemText from "@mui/material/ListItemText";
-import MenuIcon from "@mui/icons-material/Menu";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import Collapse from "@mui/material/Collapse";
 import Button from "@mui/material/Button";
 import Paper from "@mui/material/Paper";
+import Layout from "../../component/NavBar";
 
-const drawerWidth = 240;
-
-interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
-  window?: () => Window;
-}
-
-export default function ResponsiveDrawer(props: Props) {
-  const { window } = props;
-  const [open, setOpen] = React.useState(true);
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-   const handleClick = () => {
-     setOpen(!open);
-   };
-
-  const drawer = (
-    <div>
-      <Toolbar />
-      <Divider />
-      <List>
-        <ListItemButton onClick={handleClick}>
-          <ListItemText primary="Get Started" />
-          {open ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List component="div" disablePadding>
-            <ListItemButton className="pl-4">
-              <ListItemText primary="Installation" />
-            </ListItemButton>
-            <ListItemButton className="pl-4">
-              <ListItemText primary="Teams" />
-            </ListItemButton>
-          </List>
-        </Collapse>
-      </List>
-      <Divider />
-    </div>
-  );
-
-  const container =
-    window !== undefined ? () => window().document.body : undefined;
+export default function Install() {
 
   return (
-    <div className="flex ml-2 mr-2">
-    <Box className="sm-sm md-md lg-lg xl-xl">
+    <div className="flex m-1 ml-16">
+    <Box className="ms-19 ml-24 sm-sm md-md lg-lg xl-xl">
       <CssBaseline />
-      <div>
-      <AppBar
-        position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            className="mr-2 hidden sm:block"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className="text-white flex sm-sm md-md lg-lg xl-xl" noWrap component="div">
-            Welcome to Maverick
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      </div>
-      <Box
-        component="nav"
-        className="w-full sm-sm flex-shrink-0"
-        aria-label="mailbox folders"
-      >
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: "block", sm: "none" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: "none", sm: "block" },
-            "& .MuiDrawer-paper": {
-              boxSizing: "border-box",
-              width: drawerWidth,
-            },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
-      >
-        <Toolbar />
-        <Typography paragraph className="text-black text-canter">
+      <Layout></Layout>
+        <Typography paragraph className="text-black">
           You can clone Maverick from our Git repository and merge updates at
           any time.
         </Typography>
@@ -191,8 +64,8 @@ export default function ResponsiveDrawer(props: Props) {
             >
               git remote rename origin maverick
             </Typography>
-          </Paper>
-        </Box>
+          </Paper> 
+        </Box> 
 
         <br></br>
         <Typography variant="h5" className="text-black" gutterBottom>
@@ -265,7 +138,6 @@ export default function ResponsiveDrawer(props: Props) {
           </Paper>
         </Box>
         <br></br>
-      </Box>
     </Box>
     </div>
   );
