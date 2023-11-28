56a4d44d86ee3141b44733081badde3687a04375
sathishjuhomi <sathishk@juhomi.com>
2023-11-28 15:21:57 +0530

    update the email template

--- backend/utils/email_team_member.py
+++ backend/utils/email_team_member.py
@@ -1,16 +1,14 @@
 from fastapi_mail import FastMail, MessageSchema
 from jinja2 import Environment, FileSystemLoader
-from backend.utils.utils import PASSWORD_REST_TOKEN_EXPIRY
 from backend.utils.email_utils import conf, FRONTEND_SERVER_HOST, EMAIL_TEMPLATES_DIR
 
 
 async def send_invitation_email(email_to: str, invite_token: str) -> None:
-    valid_hours = PASSWORD_REST_TOKEN_EXPIRY / 60
     link = f"{FRONTEND_SERVER_HOST}/accept-invitation?token={invite_token}"
     env = Environment(loader=FileSystemLoader(EMAIL_TEMPLATES_DIR))
     template = env.get_template("accept_team_invitation.html")
     formatted_template = template.render(
-        email=email_to, link=link, valid_hours=valid_hours
+        email=email_to, link=link
     )
     message = MessageSchema(
         subject="Team Invitation",
